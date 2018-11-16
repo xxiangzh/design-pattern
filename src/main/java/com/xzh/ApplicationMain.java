d16470b249c7c366c1d55d68620b2455ad25c5e5
xzh <anenan@qq.com>
2018-11-16 10:47:41 +0800

    first commit

---
+++ src/main/java/com/xzh/ApplicationMain.java
@@ -0,0 +1,16 @@
+package com.xzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author 向振华
+ * @date 2018/11/16 10:16
+ */
+@SpringBootApplication
+public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+}
